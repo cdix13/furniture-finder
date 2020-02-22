2d3be13c4a4d171f2aa007112590fcda049b2fc5
cdix1313 <sidik.saepudin13@gmail.com>
2020-02-22 20:42:16 +0700

    Updates

---
+++ precache-manifest.07f0f412da9d29eb393e44b1bedd8bac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bfb6d36fd6e8f57474a91883ffd02c",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "94573b22aee6c0c1be93",
+    "url": "/furniture-finder/static/js/2.8b7f17b3.chunk.js"
+  },
+  {
+    "revision": "02825bf48956ceff0441792bf236c0e5",
+    "url": "/furniture-finder/static/js/2.8b7f17b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c895294e112e1273794",
+    "url": "/furniture-finder/static/js/main.add91513.chunk.js"
+  },
+  {
+    "revision": "4e4dfe4c7fc381bbbb69",
+    "url": "/furniture-finder/static/js/runtime-main.2ba4cd0a.js"
+  }
+]);
